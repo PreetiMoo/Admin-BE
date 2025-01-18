bf63ae44ba70916c9c84525e1240cbd24cca2f5f
PreetiMoo <preetissmoolya15@gmail.com>
2025-01-18 19:23:19 +0530

    cors

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true,
 }));
 
 app.options('*', cors()); 
