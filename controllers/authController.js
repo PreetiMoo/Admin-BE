9a0c5ad3c9810a4c8cae546dc5204918167e4064
PreetiMoo <preetissmoolya15@gmail.com>
2025-01-18 21:55:44 +0530

    cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,6 +74,7 @@ const loginUser = async (req, res) => {
           httpOnly: true, 
           secure: process.env.NODE_ENV === 'production', 
           maxAge: 24 * 60 * 60 * 1000, 
+          sameSite: 'None',
         })
         .send({ message: 'Login successful' , user});
     } catch (error) {
